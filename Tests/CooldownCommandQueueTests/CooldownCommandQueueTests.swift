281bcedba2f30cf4e9508ea4108ef4f9c03125dc
Michael Redig <mredig@gmail.com>
2020-03-07 15:47:13 -0600

    (feat) added basic command queue test

--- Tests/CooldownCommandQueueTests/CooldownCommandQueueTests.swift
+++ Tests/CooldownCommandQueueTests/CooldownCommandQueueTests.swift
@@ -4,7 +4,7 @@ import XCTest
 import NetworkHandler
 
 class CommandQueueTests: XCTestCase {
-	func testCommandQueue() {
+	func testCommandQueueWithRNG() {
 		let url = URL(string: "http://localhost:8000")!
 		let request = url.request
 
@@ -14,9 +14,8 @@ class CommandQueueTests: XCTestCase {
 		var timeTotal: Double = 0
 
 
-		let iterations = 10000
+		let iterations = 1000
 		for i in 0..<iterations {
-//			var thisTime = TimeInterval.random(in: 0.00...0.01)
 			let thisTime: TimeInterval = 0.001
 			var mockSuccess = NetworkMockingSession { (request) -> (Data?, Int, Error?) in
 				let someInfo: [String: Any] = [
@@ -64,4 +63,46 @@ class CommandQueueTests: XCTestCase {
 
 		XCTAssertGreaterThanOrEqual(Date(), startTime.addingTimeInterval(timeTotal))
 	}
+
+	func testCommandQueue() {
+		let commandQueue = CooldownCommandQueue()
+
+		let expecter = expectation(description: "Finished net mock")
+
+		var complete1 = false
+		var complete2 = false
+		let cooldown = TimeInterval(1)
+		var op1Completion: Date?
+
+		let operation = CooldownCommandOperation { cooldownCompletion in
+			complete1 = true
+			cooldownCompletion(cooldown, true)
+			op1Completion = Date()
+		}
+
+		let operation2 = CooldownCommandOperation { cooldownCompletion in
+			XCTAssertEqual(complete1, true)
+			XCTAssertEqual(complete2, false)
+			guard let op1completion = op1Completion else {
+				XCTFail("Did not complete first operation before second")
+				return
+			}
+			XCTAssertGreaterThanOrEqual(Date().timeIntervalSince1970, op1completion.timeIntervalSince1970 + cooldown)
+			complete2 = true
+			cooldownCompletion(0, true)
+			expecter.fulfill()
+		}
+
+		commandQueue.addTask(operation)
+		commandQueue.addTask(operation2)
+
+		waitForExpectations(timeout: 1.11) { (error) in
+			if let error = error {
+				XCTFail("Timed out waiting for an expectation: \(error)")
+			}
+		}
+
+
+	}
+
 }
